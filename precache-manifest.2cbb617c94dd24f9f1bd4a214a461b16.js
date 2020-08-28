a2907443bdf720c9110cffe6c80ad88083924516
Flo Orford <flo.orford@hotmail.com>
2020-08-28 12:18:42 +0200

    Updates

---
+++ precache-manifest.2cbb617c94dd24f9f1bd4a214a461b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7585421fbb09b6560e84925daf9e6c0",
+    "url": "/teaching_app/index.html"
+  },
+  {
+    "revision": "ccb98730ebb75ef95dde",
+    "url": "/teaching_app/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "ccb98730ebb75ef95dde",
+    "url": "/teaching_app/static/js/2.576450e5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/teaching_app/static/js/2.576450e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9689fcfd618911c1c1",
+    "url": "/teaching_app/static/js/main.9b9e2211.chunk.js"
+  },
+  {
+    "revision": "815d484b489862958e21",
+    "url": "/teaching_app/static/js/runtime-main.205e80e8.js"
+  }
+]);
